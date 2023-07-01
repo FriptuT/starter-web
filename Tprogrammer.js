0a071fd0c82374e1b759a8bf19383d2cfe0749a5
Friptu Teodor <friptut@gmail.com>
2023-07-01 19:22:45 +0300

    insert more comments

---
+++ Tprogrammer.js
@@ -0,0 +1,16 @@
+// This is an in-line comment
+/*
+    This is a multi-line comment
+*/
+var parfume = "Stronger with you - Giorgio Armani"
+var priceRON = 80
+var myBugetRON = 250
+
+function calculateChange(a,b)
+{
+    var rest;
+    rest = a - b;
+    return rest;
+}
+
+calculateChange(myBugetRON, priceRON);
